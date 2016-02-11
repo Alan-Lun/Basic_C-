36fe14e0daed4e85bbde52d46867da3d36d67dfb
黃振倫 <s52076663135@gmail.com>
2016-02-11 16:45:42 +0800

    4.屬性與修飾子

--- BasicCSherp_Console/Program.cs
+++ BasicCSherp_Console/Program.cs
@@ -29,6 +29,13 @@ namespace BasicCSherp_Console
             A2 _PC3 = new A2();
             string _strPC3 = _PC3.Happy("自爽");
 
+            //--------------------------------------------------
+
+            //A4 屬性與修飾子
+            A4 _PC4 = new A4();
+            _PC4.ReStartNum = 5;
+            Console.WriteLine(_PC4.ReStart());
+            Console.Read();
 
         }
 
@@ -74,6 +81,57 @@ namespace BasicCSherp_Console
 
 
         }
+
+        /// <summary>
+        /// A4-屬性與修飾子
+        /// </summary>
+        public class A4
+        {
+            //修飾子
+            //public 好比家裡完全透明，家具都看得到，都可以動
+            //private 就像是家裡有牆壁保護住，只有我們才可以動裡面的家具
+
+            //屬性，可設定成唯獨或者是條件式設定
+            private int _ReStartNum;
+            
+            public int ReStartNum
+            {
+                get { return _ReStartNum; }
+                // set { _ReStartNum = value; }
+                set {
+                    if(value<=5)
+                    {
+                        _ReStartNum = value;
+                    }
+                    else
+                    {
+                        _ReStartNum = 5;
+                    }
+                    
+
+
+                }
+
+
+            }
+
+            public string ReStart()
+            {
+                string result = "";
+                for (int i = 0; i < ReStartNum; i++)
+                {
+                    result += "重開 ";
+                }
+                return result;
+
+
+
+            }
+
+
+
+        }
+
 
 
 
